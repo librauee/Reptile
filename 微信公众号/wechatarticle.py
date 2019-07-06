2fddfff4d725438b59986899264444cd6b821f0d
librauee <870407139@qq.com>
2019-07-06 14:10:05 +0800

    爬取公众号文章入库

--- 微信公众号/wechatarticle.py
+++ 微信公众号/wechatarticle.py
@@ -10,14 +10,14 @@ import json
 import time
 from pymongo import MongoClient
 
-url = 'http://mp.weixin.qq.com/mp/profile_ext'  
+url='http://mp.weixin.qq.com/mp/profile_ext'  
 
 # Mongo配置
-conn = MongoClient('127.0.0.1', 27017)
-db = conn.wx                    #连接wx数据库，没有则自动创建
-mongo_wx = db.article           #使用article集合，没有则自动创建
+conn=MongoClient('127.0.0.1', 27017)
+db=conn.wx                    #连接wx数据库，没有则自动创建
+mongo_wx=db.article           #使用article集合，没有则自动创建
 
-def get_wx_article(biz, uin, key, count=10):
+def get_wx_article(biz,uin,key,pass_ticket,appmsg_token,count=10):
     offset=1+(index+1)*11
     params={
         '__biz':biz,
@@ -27,10 +27,10 @@ def get_wx_article(biz, uin, key, count=10):
         'count':count,
         'action':'getmsg',
         'f':'json',
-        'pass_ticket':'haTp/uxAvK6S4PyD2Vy3CJNGjC17qUsEFNGh6aBl4Dac2VBOS0qv8bLEPj+ckE8m',
+        'pass_ticket':pass_ticket,
         'scene':124,
         'is_ok':1,
-        'appmsg_token':'1016_DCj%2Bn0mSBMHcXTFWuRccLnEJTBvaFcz0MWozQQ~~',
+        'appmsg_token':appmsg_token,
         'x5':0,
     }
 
@@ -43,24 +43,24 @@ def get_wx_article(biz, uin, key, count=10):
     #print(resp_json)
     if resp_json.get('errmsg') == 'ok':
         # 是否还有分页数据，若没有更多数据则返回
-        can_msg_continue = resp_json['can_msg_continue']
+        can_msg_continue=resp_json['can_msg_continue']
         # 当前分页文章数
-        msg_count = resp_json['msg_count']
+        msg_count=resp_json['msg_count']
         print("当前分页共有{}篇文章".format(msg_count))
-        general_msg_list = json.loads(resp_json['general_msg_list'])
-        infolist = general_msg_list.get('list')
+        general_msg_list=json.loads(resp_json['general_msg_list'])
+        infolist=general_msg_list.get('list')
         print(infolist, "\n**************")
         for info in infolist:
-            app_msg_ext_info = info['app_msg_ext_info']
+            app_msg_ext_info=info['app_msg_ext_info']
             # 标题
-            title = app_msg_ext_info['title']
+            title=app_msg_ext_info['title']
             # 文章链接
-            content_url = app_msg_ext_info['content_url']
+            content_url=app_msg_ext_info['content_url']
             # 封面图
-            cover = app_msg_ext_info['cover']
+            cover=app_msg_ext_info['cover']
             # 发布时间
-            datetime = info['comm_msg_info']['datetime']
-            datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(datetime))
+            datetime=info['comm_msg_info']['datetime']
+            datetime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(datetime))
 
             mongo_wx.insert({
                 'title': title,
@@ -68,7 +68,7 @@ def get_wx_article(biz, uin, key, count=10):
                 'cover': cover,
                 'datetime': datetime
             })
-        if can_msg_continue == 1:
+        if can_msg_continue==1:
             return True
         return False
     else:
@@ -77,16 +77,19 @@ def get_wx_article(biz, uin, key, count=10):
 
 
 if __name__ == '__main__':
-    biz = 'MzIwNDI1NjUxMg=='
-    uin = 'MTY1NTcyMDYxMg=='
-    key = '4af267131119af888dfd338094ba8aa1afba149aadd84f9adb525d84ab5afbd3bbf4289e2fad9ef2fc29954c3de21074b0116fd47fdcc67c1dd905a0e2ff6942b0544ff041ac366c08e1a53247b4fd57'
-    index = -1
+    # 参数通过抓包获得
+    biz=''
+    uin=''
+    key=''
+    pass_ticket=''
+    appmsg_token=''
+    index=-1
     while 1:
         print(f'开始抓取公众号第{index + 1} 页文章.')
-        flag = get_wx_article(biz, uin, key,index=index)
+        flag=get_wx_article(biz,uin,key,pass_ticket,appmsg_token,index=index)
         # 防止和谐，暂停8秒
         time.sleep(8)
-        index += 1
+        index+=1
         if not flag:
             print('该公众号文章已全部抓取并且存入本地数据库')
             break
