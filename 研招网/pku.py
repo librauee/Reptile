6e37f2851f6b7a725e9a30911a44afc9ad0da10a
librauee <870407139@qq.com>
2019-07-11 10:05:02 +0800

    爬取研招网调剂信息

---
+++ 研招网/pku.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 20:18:13 2019
+
+@author: Administrator
+
+无robots协议
+
+Not Found
+The requested URL /robots.txt was not found on this server.
+
+[<li class="fl"><a class="fl" href="https://admission.pku.edu.cn/xxgk/xxgkssbm/cscj/16849dbbd425dbb5caf02d799ea1a3d2/cscj_ss_index.html" target="_blank" title="北京大学2019年硕士研究生招生考试初试成绩">            北京大学2019年硕士研究生招生考试初试成绩 
+      </a><span class="fr">2019-02-15</span></li>
+      
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+import random
+import time
+import numpy as np
+
+def getHTMLText(url,headers):
+    try:
+        r=requests.get(url,headers=headers,timeout=30)
+        r.raise_for_status()
+        r.encoding=r.apparent_encoding
+        return r.text
+    except:
+        return "爬取失败"
+    
+def parsehtml(urllist,html):
+    soup=BeautifulSoup(html,'html.parser')
+    t=soup.find_all('a',attrs={'class':'fl'},limit=3)
+    #print(t)
+    for i in t:
+        urllist.append(i.get('href'))
+
+def getpdf(url):
+    realurl='https://admission.pku.edu.cn/'+url
+    r=requests.get(realurl)
+    pdf=r.content
+    with open('复试分数线.pdf','wb') as f:
+        f.write(pdf)
+    print("finish downloading")
+        
+def send(urllist,flag):
+    url='https://admission.pku.edu.cn/xxgk/xxgkssbm/cscj/16849dbbd425dbb5caf02d799ea1a3d2/cscj_ss_index.html'
+    if urllist[0]!=url:
+        print("开始发送邮件")
+        sendemail(urllist[0])
+        getpdf(urllist[0])
+        flag=False
+    else:
+        print('Nothing new')
+        
+    return flag
+        
+def sendemail(url):
+
+    msg_from='870407139@qq.com'                                 #发送方邮箱
+    passwd='                '                                   #填入发送方邮箱的授权码
+    receivers=['870407139@qq.com,503734057@qq.com']             #收件人邮箱
+                            
+    subject="北大研招网更新连接"                                     #主题     
+    content="根据本爬虫日以继夜的爬取北大研招网，发现有最新更新链接：https://admission.pku.edu.cn/"+url                                                     #正文
+    msg = MIMEText(content)
+    msg['Subject'] = subject
+    msg['From'] = msg_from
+    msg['To'] = ','.join(receivers)
+    try:
+        s=smtplib.SMTP_SSL("smtp.qq.com",465)                   #邮件服务器及端口号
+        s.login(msg_from, passwd)
+        s.sendmail(msg_from, msg['To'].split(','), msg.as_string())
+        print("发送成功")
+    except:
+        print("发送失败")
+    finally:
+        s.quit()
+
+
+    
+    
+def main():
+    #北大研招网链接
+    url = 'https://admission.pku.edu.cn/'
+    user_agent = [
+    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; InfoPath.3; rv:11.0) like Gecko",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+    "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+    "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+    "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+    "Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+    "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+    "Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+    "MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+    "Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10",
+    "Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13",
+    "Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+",
+    "Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 Safari/534.6 TouchPad/1.0",
+    "Mozilla/5.0 (SymbianOS/9.4; Series60/5.0 NokiaN97-1/20.0.019; Profile/MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/525 (KHTML, like Gecko) BrowserNG/7.1.18124",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; HTC; Titan)",
+    "UCWEB7.0.2.37/28/999",
+    "NOKIA5700/ UCWEB7.0.2.37/28/999",
+    "Openwave/ UCWEB7.0.2.37/28/999",
+    "Mozilla/4.0 (compatible; MSIE 6.0; ) Opera/UCWEB7.0.2.37/28/999",
+     # iPhone 6：
+    "Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25",
+ 
+]
+    #随机选取头部 代理
+
+    headers = {'User-Agent': random.choice(user_agent),
+               'Accept-Encoding':'gzip, deflate, br',
+               'Accept-Language':'zh-CN,zh;q=0.9',
+               'Connection':'keep-alive',
+               'Cookie':'__guid=177253749.2024323391817798000.1551141126695.8904'}
+    count=0
+    flag=True
+    while flag:
+        count+=1
+        print(count, 'times')      # 输出运行了多少次
+        html = getHTMLText(url, headers)
+        newlist=[]
+        parsehtml(newlist,html)
+        #print(newlist)
+        flag=send(newlist,flag)
+        rdtime=100*np.random.rand()
+        time.sleep(rdtime)         # 间隔随机百秒内时间执行
+        if flag==False:
+            break
+    print("Over!")
+
+                
+    
+
+
+
+if __name__ == '__main__':
+    main()
