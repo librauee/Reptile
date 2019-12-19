01206030e08c39f8bec7fec7a021b7dd5be1d735
librauee <870407139@qq.com>
2019-12-19 20:45:22 +0800

    云顶之奕英雄信息爬取

--- 云顶之奕/yunding.py
+++ 云顶之奕/yunding.py
@@ -9,6 +9,7 @@ import requests
 import re
 import demjson
 from pymongo import MongoClient
+import pandas as pd
 
 class crawl_hero(object):
     
@@ -19,39 +20,99 @@ class crawl_hero(object):
                 'Referer': 'http://lol.duowan.com/1906/m_425042631560.html',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
                 }
-        self.db=MongoClient().lol
+        self.db=MongoClient().lol1
+        self.color_gold={ 
+                'white':1,
+                'green':2,
+                'blue':3,
+                'purple':4,
+                'gold':5,
+                'colorful':7
+                }
+        self.sort_info={
+            'heroSort0':'剧毒',
+            'heroSort2':'云霄',
+            'heroSort3':'光',
+            'heroSort4':'影',
+            'heroSort5':'地狱火',
+            'heroSort6':'森林',
+            'heroSort7':'海洋',
+            'heroSort8':'沙漠',
+            'heroSort9':'钢铁',
+            'heroSort10':'雷霆',
+            'heroSort11':'山脉',
+            'heroSort12':'水晶',
+            'heroSort13':'极地',
+            'heroJob0':'刺客',
+            'heroJob2':'剑士',
+            'heroJob3':'游侠',
+            'heroJob4':'秘术',
+            'heroJob5':'法师',
+            'heroJob6':'大元素使',
+            'heroJob7':'狂战士',
+            'heroJob8':'守护神',
+            'heroJob9':'掠食者',
+            'heroJob10':'召唤使',
+            'heroJob11':'德鲁伊',
+            'heroJob12':'炼金师',
+            
+            }
         
     def get_json(self):
         
         r=requests.get(self.url,headers=self.headers)
-#        a=re.sub('(// .*?\n)','',r.text)
-#        a=re.sub('//purple,white, gold, blue, green','',a)
         json1=demjson.decode(r.text[9:-1])
-
-        sorts_data=json1['hero']['sorts']
-        for sorts in sorts_data:
-            self.db['sorts'].insert_one(sorts)
-        jobs_data=json1['hero']['jobs']
-        for jobs in jobs_data:
-            self.db['jobs'].insert_one(jobs)
         hero_data=json1['hero']['data']
+        count=0
         for hero in hero_data:
+            info=[]
+            hero_id=hero['id']
             color=hero['color']
+            gold=self.color_gold[color]
             job=hero['job']
-            name=hero['name']
-            skill=hero['skill']
+            job=job.split(',')
+            name=hero['name']   
+            name=name.replace(","," ")
+            name=name.split()[-1]            
+            names=name.split()
             sort=hero['sort']
+            sort=sort.split(',')
+            for i in sort:
+                info.append(self.sort_info[i])
+            for i in job:
+                info.append(self.sort_info[i])
+                
+            
             hero_dict={
-                    'color':color,
-                    'job':job,
+                    'hero_id':count,
+                    'gold':gold,
                     'name':name,
-                    'skill':skill,
-                    'sort':sort
+                    'info':info
                     }
             self.db['hero'].insert_one(hero_dict)
+            for i in range(len(names)):
+                hero_short_dict={
+                    'hero_id':count,
+                    'name':names[i],
+                    
+                    }
+            self.db['hero_short'].insert_one(hero_short_dict)
+            count+=1
+            
+    def save(self):    
+        
+        data=list(self.db['hero'].find())
+        df=pd.DataFrame(data)
+        df=df.drop('_id',axis=1)
+        df.to_csv('heros_info.csv', index=None, encoding='utf_8_sig')
+        df=pd.DataFrame(list(self.db['hero_short'].find()))
+        df=df.drop('_id',axis=1)
+        df.to_csv('heros_info_short.csv', index=None, encoding='utf_8_sig')
+
             
         
 if __name__=='__main__':
     
     crawl=crawl_hero()
     crawl.get_json()
+    crawl.save()
